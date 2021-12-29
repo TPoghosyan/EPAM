3d312c1a486a077f287554a77923f070edb17c45
Tigran Poghosyan <ttiikkooooo@gmail.com>
2021-12-29 04:20:56 -0800

    this is commit

---
+++ Tigran.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Tigran {
+    public static void main(String[] args) {
+           Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+double d = a+b+c;
+
+        System.out.println("sum = "+d);
+
+    }
+}
+
